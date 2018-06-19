0e861a273f84314b7b41f84d5454ebdfbca9b2c1
Beamanator <dabeamanator@gmail.com>
2018-06-19 22:17:18 +0200

    Update for React code / folder structure

---
+++ public/extension_code/js/optionsPageRedirect.js
@@ -0,0 +1,6 @@
+//--------------------------------------------------------------
+//     NOTE: ONLY EDIT CODE IN /public/* - NOT IN /build/*
+//--------------------------------------------------------------
+
+// The only purpose of this popup is to redirect user to the Options page
+chrome.runtime.openOptionsPage();
