27810546b133b1dccf7091016fc5c7d5e8873c7e
Beamanator <dabeamanator@gmail.com>
2018-06-21 14:48:34 +0200

    Connected Main to store & add logout button

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Button from '../../components/UI/Button/Button';
+import * as actions from '../../store/actions/index';
 
 import classes from './Main.css';
 
 class Main extends Component {
+    logoutHandler = () => {
+        console.log('logout');
+        this.props.onLogout();
+    }
+
     render () {
+        const logoutBtn = (
+            <Button
+                btnType="Danger"
+                clicked={this.logoutHandler}
+            >LOGOUT</Button>
+        );
+
         return (
             <div>
-                Main content here
+                <p>WHEN DONE, PLEASE {logoutBtn}</p>
+                <p>Main content here</p>
             </div>
         );
     }
 }
 
-export default Main;
+// const mapStateToProps = state => {}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onLogout: () => dispatch(actions.logout())
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Main);
