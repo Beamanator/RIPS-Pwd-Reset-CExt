949964be7e4799614a3b835296f4271452011992
Beamanator <dabeamanator@gmail.com>
2018-06-21 22:47:22 +0200

    connected to redux, init background port

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+/*global chrome*/
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Auth from './containers/Auth/Auth';
 import Main from './containers/Main/Main';
+import Spinner from './components/UI/Spinner/Spinner';
 
 import * as actions from './store/actions/index';
 import classes from './App.css';
@@ -10,6 +12,18 @@ import classes from './App.css';
 class App extends Component {
     componentDidMount() {
         this.props.onTryAutoSignin();
+
+        // if no port exists, set one up!
+        if (!this.props.bkgPort) {
+            // store port in redux store
+            this.props.onBackgroundPortInit(chrome);
+        } else {
+            console.warn('<App> port already exists', this.props.bkgPort);
+        }
+    }
+
+    componentWillUnmount() {
+        // TODO: destroy the background port - call action!
     }
     
     render() {
@@ -33,13 +47,15 @@ class App extends Component {
 
 const mapStateToProps = state => {
     return {
-        isAuthenticated: state.auth.token !== null
+        isAuthenticated: state.auth.token !== null,
+        bkgPort: state.port.port
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onTryAutoSignin: () => dispatch(actions.authCheckState())
+        onTryAutoSignin: () => dispatch(actions.authCheckState()),
+        onBackgroundPortInit: (chrome) => dispatch(actions.backgroundPortInit(chrome))
     };
 };
 
