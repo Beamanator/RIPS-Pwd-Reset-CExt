10b1bbbab940005022fe8ecc7917d875279df83a
Beamanator <dabeamanator@gmail.com>
2018-06-24 15:00:10 +0200

    Now displays dynamic prop data

--- src/components/Table/Row/Row.js
+++ src/components/Table/Row/Row.js
@@ -2,19 +2,26 @@ import React from 'react';
 
 import classes from './Row.css';
 
+const wrapCell = (type, content) => {
+    // if no content, set to empty str
+    if (!content) content = '';
+    
+    if (type === 'header') {
+        return <th>{content}</th>;
+    } else {
+        return <td>{content}</td>;
+    }
+};
+
 const row = (props) => {
-    const wrapCell = content => {
-        if (props.type === 'header') {
-            return <th>{content}</th>;
-        } else {
-            return <td>{content}</td>;
-        }
-    };
+    // if data not passed, set empty obj
+    const data = props.data ? props.data : {};
 
     return (
         <tr className={classes.Container}>
-            {wrapCell( 'Cell 1' )}
-            {wrapCell( 'Cell 2!' )}
+            {wrapCell( props.type, data.username )}
+            {wrapCell( props.type, data.email )}
+            {wrapCell( props.type, data.lastPwdChange )}
         </tr>
     );
 };
