4e02f2dc063b2529becd42cd02fb8331c0dcfe17
Beamanator <dabeamanator@gmail.com>
2018-06-26 15:12:33 +0200

    Adding portCodes to store

---
+++ src/store/portCodes.js
@@ -0,0 +1,15 @@
+// ==============================================================================
+//                           CODES FOR REACT APP FILES
+// ==============================================================================
+
+// constant to hold the name of the react app / background.js port
+export const REACT_APP_PORT = 'REACT_APP_PORT';
+
+// receive codes
+export const INIT_PORT = 'INIT_PORT';
+export const ERROR_CODE_NOT_RECOGNIZED = 'ERROR_CODE_NOT_RECOGNIZED';
+export const USER_DATA_PAYLOAD = 'USER_DATA_PAYLOAD';
+
+// send codes (to background.js)
+export const START_IMPORT = 'START_IMPORT';
+export const CONTINUE_IMPORT = 'CONTINUE_IMPORT';
