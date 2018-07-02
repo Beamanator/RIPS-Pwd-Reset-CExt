e93a3c0384c2c5454ebe4dfe6f5639cd6495c21d
Beamanator <dabeamanator@gmail.com>
2018-07-02 13:45:27 +0200

    Added comments to fns, added string sort array method

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,8 @@
+/**
+ * Returns a copy of the old object, with new properties from updatedProperties obj
+ * @param {object} oldObject 
+ * @param {object} updatedProperties 
+ */
 export const updateObject = (oldObject, updatedProperties) => {
     return {
         ...oldObject,
@@ -5,6 +10,11 @@ export const updateObject = (oldObject, updatedProperties) => {
     };
 };
 
+/**
+ * Checks validity of input value based on rules
+ * @param {number || string} value 
+ * @param {object} rules - array of rules 
+ */
 export const checkValidity = ( value, rules ) => {
     let isValid = true;
 
@@ -35,4 +45,41 @@ export const checkValidity = ( value, rules ) => {
     }
 
     return isValid;
+}
+
+/**
+ * Helper function to sortStringArr - abstraction of the key array's reduce fn
+ * @param {object} obj - accumulatory object
+ * @param {string} key - name of key to look for in object
+ */
+const sortStringArrReduce = (obj, key) => {
+    if (!obj[key]) {
+        console.error(`<sortStringArr> key [${key}] not defined in:`, obj);
+        return {};
+    } else return obj[key];
+}
+/**
+ * Sorts an array of strings - can search through objects if passing in keyArr
+ * @param {object} strArr - array of strings / objects with strings
+ * @param {object} [keyArr=[]] - array of keys to get to sort values (ex: ['username'])
+ */
+export const sortStringArr = (strArr, keyArr=[]) => {
+    const newArr = [...strArr]; // create new copy of shell array
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    return newArr.sort((elemA, elemB) => {
+        // get strings from object elems
+        const strA = keyArr.reduce(sortStringArrReduce, elemA).toUpperCase();
+        const strB = keyArr.reduce(sortStringArrReduce, elemB).toUpperCase();
+
+        // console.log(elemA, elemB, strA, strB);
+        
+        if (strA > strB) {
+            return 1;
+        }
+        if (strA < strB) {
+            return -1;
+        }
+        return 0;
+    });
 }
